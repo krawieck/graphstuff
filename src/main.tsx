a0833cb7939ffe65eeb42143b0a4a3689d93348c
Filip Krawczyk <fil.krawczyk@gmail.com>
2023-06-28 10:57:20 +0200

    update extension stuff

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./sections/App"
 
+import "./util"
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
--- src/util.ts
+++ src/util.ts
@@ -1,18 +1,4 @@
-interface Math {
-  /** num^2 */
-  pow2(num: number): number
-}
-
 Math.pow2 = (num: number) => num * num
-
-interface Array<T> {
-  /** Math.min of list of elements */
-  min(): T
-  /** Math.max of list of elements */
-  max(): T
-  /** pops an element at [index] and returns it */
-  popAt(index: number): void
-}
 
 Array.prototype.min = function () {
   return Math.min(...this)
