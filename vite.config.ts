7002e3525db723c458c7e462fa7a35b331e99a57
Filip Krawczyk <fil.krawczyk@gmail.com>
2025-07-27 21:04:27 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // comment out the following line if you want to run tauri. this is here for GitHub Pages
   base: "/graphstuff/",
 })
