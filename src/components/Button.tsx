6b29b812fa56ecd6369e339437c597a9fe62cb0d
krawieck <fil.krawczyk@gmail.com>
2025-07-27 18:31:51 +0000

    Deploying to gh-pages from @ krawieck/graphstuff@7d90faa688aa2543574b861003c1036e8513d35f 🚀

--- src/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-export const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (
-  args
-) => {
-  return (
-    <button className="rounded-full bg-pink-600 px-4 py-1" {...args}></button>
-  )
-}
